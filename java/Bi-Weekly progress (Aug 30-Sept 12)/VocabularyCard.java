918c876c8c0580b6ac973443f8d391e09e77b9c2
Swarfte <wcpbenchau@gmail.com>
2021-08-29 15:15:08 +0800

    整體完成 加入了隨機功能

--- java/Bi-Weekly progress (Aug 30-Sept 12)/VocabularyCard.java
+++ java/Bi-Weekly progress (Aug 30-Sept 12)/VocabularyCard.java
@@ -1,8 +1,8 @@
 /*
- * @Author: Swarfte
+ * @Author: Chau Lap Tou
  * @Date: 2021-08-25 22:54:39
- * @LastEditors: Swarfte
- * @LastEditTime: 2021-08-29 13:19:51
+ * @LastEditors: Chau Lap Tou
+ * @LastEditTime: 2021-08-29 15:14:54
  * @python_exe: pyinstaller -F -w file_name.py -p C:/python/lib/site-packages 
  * @java_class: javac -encoding utf-8 file_name.java
  * @java_jar: jar -cvmf manifest.txt name.jar *.class
@@ -25,23 +25,27 @@ public class VocabularyCard{
 }
 
 class VocabularyCardGUI{
-    int width = 1000;//寬度
-    int height = 400;//高度
-    int fontSize = 125;//字體大小
-    JFrame mainFrame = new JFrame("英文生字卡");//主框架
-    JPanel mainPanel = new JPanel();//主畫面
-    JLabel wordText = new JLabel();//顯示生字
-    JButton changeButton = new JButton("change");//切換中文英按鈕
-    JButton nextButton = new JButton("next");//下一個生字按鈕
-    JButton lastButton = new JButton("last");//上一個生字按鈕
-    JPanel buttonPanel = new JPanel();//放按鈕用的布局
-    String fileName = "word.properties";//*存放單詞的檔案
-    Properties wordInput = new Properties();//讀取單詞用
-    ArrayList < String > chineseWord = new ArrayList < >();//存放單詞的中文
-    ArrayList< String > englishWord = new ArrayList < >();//存放單詞的英文
-    boolean isChinese = true;//檢測目前的生字卡狀態
-    int CurrentNumber = 0;//目前的生字位置
-    int wordLength ;//檢測有多少個生字
+    public int width = 1200;//寬度
+    public int height = 400;//高度
+    public Font textFont = new Font ("Dialog",1,130);//顯示文本的字體
+    private JFrame mainFrame = new JFrame("英文生字卡");//主框架
+    private JPanel mainPanel = new JPanel();//主畫面
+    private JLabel wordText = new JLabel();//顯示生字
+    public JButton changeButton = new JButton("change");//切換中文英按鈕
+    public JButton nextButton = new JButton("next");//下一個生字按鈕
+    public JButton lastButton = new JButton("last");//上一個生字按鈕
+    public JButton randomButton = new JButton("random");//隨機生字按鈕
+    private ArrayList < JButton > buttonList = new ArrayList<>();//存放按鈕的列表
+    public Dimension buttonSize = new Dimension(150,50);//設置按鈕大小
+    public Font buttonFont = new Font("Dialog",1,30);//按鈕字體的大小
+    private JPanel buttonPanel = new JPanel();//放按鈕用的界面
+    public  String fileName = "word.properties";//*存放單詞的檔案
+    private Properties wordInput = new Properties();//讀取單詞用
+    private ArrayList < String > chineseWord = new ArrayList < >();//存放單詞的中文
+    private ArrayList< String > englishWord = new ArrayList < >();//存放單詞的英文
+    public boolean isChinese = true;//檢測目前的生字卡狀態
+    private int currentNumber = 0;//目前的生字位置
+    private int wordLength ;//檢測有多少個生字
 
     public void loadDictionary() {
         try {
@@ -66,23 +70,29 @@ class VocabularyCardGUI{
     }
 
     public void buttonSet(){
-        lastButton.addActionListener(new lastListener());
-        buttonPanel.add(lastButton);
+        buttonList.add(lastButton);
+        buttonList.add(changeButton);
+        buttonList.add(nextButton);
+        buttonList.add(randomButton);
 
+        for (JButton x : buttonList){
+            x.setPreferredSize(buttonSize);
+            x.setFont(buttonFont);
+            buttonPanel.add(x);
+        }
+
+        lastButton.addActionListener(new lastListener());
         changeButton.addActionListener(new changeListener());
-        buttonPanel.add(changeButton);
-        
         nextButton.addActionListener(new nextListener());
-        buttonPanel.add(nextButton);
+        randomButton.addActionListener(new randomListener());
 
         buttonPanel.setBackground(Color.darkGray);
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
         mainFrame.getContentPane().add(BorderLayout.SOUTH,buttonPanel);
     }
 
     public void textSet() {
-        wordText.setFont(new Font("Dialog",1,fontSize));
-        wordText.setText(chineseWord.get(CurrentNumber));
+        wordText.setFont(textFont);
+        wordText.setText(chineseWord.get(currentNumber));
         mainPanel.add(wordText);
         mainFrame.getContentPane().add(BorderLayout.CENTER,mainPanel);
     }
@@ -97,10 +107,10 @@ class VocabularyCardGUI{
     class changeListener implements ActionListener {
         public void actionPerformed(ActionEvent ae){
             if (isChinese){
-                wordText.setText(englishWord.get(CurrentNumber));
+                wordText.setText(englishWord.get(currentNumber));
                 isChinese = false;
             }else {
-                wordText.setText(chineseWord.get(CurrentNumber));
+                wordText.setText(chineseWord.get(currentNumber));
                 isChinese = true;
             }
         }
@@ -108,19 +118,19 @@ class VocabularyCardGUI{
     
     class nextListener implements ActionListener {
         public void actionPerformed(ActionEvent ae){
-            if (CurrentNumber < wordLength - 1 ){//不在最後一個單詞的位置
+            if (currentNumber < wordLength - 1 ){//不在最後一個單詞的位置
                 if (isChinese){
-                    wordText.setText(chineseWord.get(CurrentNumber + 1));
+                    wordText.setText(chineseWord.get(currentNumber + 1));
                 }else{
-                    wordText.setText(englishWord.get(CurrentNumber + 1));
+                    wordText.setText(englishWord.get(currentNumber + 1));
                 }
-                CurrentNumber++;
+                currentNumber++;
             }else{
-                CurrentNumber = 0 ;
+                currentNumber = 0 ;
                 if (isChinese){
-                    wordText.setText(chineseWord.get(CurrentNumber));
+                    wordText.setText(chineseWord.get(currentNumber));
                 }else {
-                    wordText.setText(englishWord.get(CurrentNumber));
+                    wordText.setText(englishWord.get(currentNumber));
                 }
             }
         }
@@ -128,20 +138,31 @@ class VocabularyCardGUI{
 
     class lastListener implements ActionListener {
         public void actionPerformed(ActionEvent ae){
-            if (CurrentNumber > 0){//不在第一個單詞的位置
+            if (currentNumber > 0){//不在第一個單詞的位置
                 if (isChinese){
-                    wordText.setText(chineseWord.get(CurrentNumber - 1));
+                    wordText.setText(chineseWord.get(currentNumber - 1));
                 }else {
-                    wordText.setText(englishWord.get(CurrentNumber - 1));
+                    wordText.setText(englishWord.get(currentNumber - 1));
                 }
-                CurrentNumber--;
+                currentNumber--;
             }else {
-                CurrentNumber = wordLength - 1;
+                currentNumber = wordLength - 1;
                 if (isChinese){
-                    wordText.setText(chineseWord.get(CurrentNumber));
+                    wordText.setText(chineseWord.get(currentNumber));
                 }else {
-                    wordText.setText(englishWord.get(CurrentNumber));
+                    wordText.setText(englishWord.get(currentNumber));
                 }
+            }
+        }
+    }
+
+    class randomListener implements ActionListener{
+        public void actionPerformed(ActionEvent ae){
+            currentNumber = (int) (Math.random() * (wordLength - 1));//在可選範圍內隨機找一個生字
+            if (isChinese){
+                wordText.setText(chineseWord.get(currentNumber));
+            }else {
+                wordText.setText(englishWord.get(currentNumber));
             }
         }
     }
